8beb41140e81a4b7c35b41a267c29d4c65e6a45a
conor <conorjm89@gmail.com>
2021-11-13 18:26:44 +0000

    Simply to use express.static middleware

--- index.js
+++ index.js
@@ -4,31 +4,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-function serveIndex(req, res) {
-  res.sendFile(path.join(__dirname, '/pages/index.html'));
-}
-
-function serveAbout(req, res) {
-  res.sendFile(path.join(__dirname, '/pages/about.html'));
-}
-
-function serveContact(req, res) {
-  res.sendFile(path.join(__dirname, '/pages/contact-me.html'));
-}
+app.use(express.static('public'));
 
 function serve404(req, res) {
-  res.sendFile(path.join(__dirname, '/pages/404.html'));
+  res.sendFile(path.join(__dirname, '/public/404.html'));
 }
-
-function serveCSS(req, res) {
-  res.sendFile(path.join(__dirname, '/assets/style.css'));
-}
-
-app.get('/', serveIndex);
-app.get('/index', serveIndex);
-app.get('/about', serveAbout);
-app.get('/contact-me', serveContact);
-app.get('/assets/style.css', serveCSS);
 
 // If no middleware of routes can be used, 404
 app.use(serve404);
